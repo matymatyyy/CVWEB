8bd6b676463071534b66a165aece00171249d27b
Matias Dominguez <matydominguez554@gmail.com>
2023-04-24 19:35:18 -0300

    arreglos pequeño

--- comandos/comando.js
+++ comandos/comando.js
@@ -1,5 +1,16 @@
 function iniciar(){
-    
+    const espacio= document.getElementById("espacio")
+    espacio.style.display="hidden"
+
+    const botones= document.getElementsByClassName("boton1")
+    document.addEventListener("click", hola)
+
+    function hola(){
+        console.log("hola")
+    }
+
+
+
 }
 
 window.addEventListener("load", iniciar)
