46596da04671184628bcde1e8ea6aad7d548865f
Matias Dominguez <matydominguez554@gmail.com>
2023-04-26 21:52:06 -0300

    borro total

--- comandos/comando.js
+++ comandos/comando.js
@@ -1,14 +1,5 @@
 function iniciar(){
-    const espacio= document.getElementById("espacio")
-    const proyecto= document.getElementById("Proyectos")
-    proyecto.style.display="none"
-
-    const botones= document.getElementsByClassName("boton1")
-    document.addEventListener("click", hola)
-
-    function hola(){
-        console.log("hola")
-    }
+    
 
 
 
